49bf29791561ffb13a4b402936a0aafbbedef244
RodinDmitry <kod744@gmail.com>
2016-05-13 22:15:34 +0300

    Update and rename siteloader.sh to site/siteloader.sh

--- site/siteloader.sh
+++ site/siteloader.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-generateQuery()
-{
-	echo -e "GET " $2 " HTTP/1.1\nHost: "\ $1 "\nAccept: text/html\nConnection: close\n\n" > Query
-}
-
 echo "/" > links
 
 HOME=$1
@@ -14,8 +9,6 @@ mkdir -p $HOME
 while read line; do
 	NEWDIR=$HOME$line
 	mkdir -p $NEWDIR
-	#generateQuery $1 $line
-	#netcat $1 80 < Query > webpage
 	echo "GET $2 HTTP/1.1 \n\n" | nc $1 80 > webpage
 	cp webpage $NEWDIR$webpage
 	
