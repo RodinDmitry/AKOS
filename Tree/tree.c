09586ff396b918dae19b51cbf3cb38be09ea9260
RodinDmitry <kod744@gmail.com>
2016-05-21 02:57:48 +0300

    Update tree.c

--- Tree/tree.c
+++ Tree/tree.c
@@ -2,47 +2,48 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-unsigned char isFolder = 0x4;
-
-
-void printname(int tab, char* route)
+void printname(int tab, char* name)
 {
 	int i;
 	for (i = 0; i < tab; ++i)
 	{
-		printf ("  ");
+		printf ("	");
 	}
-	printf ("%s\n", route);
+	printf ("%s\n", name);
 }
 
-
-void function(int tab, char* route)
+void create_tree(char* dirname,int tabs)
 {
-	DIR* dir;
+	DIR* dir = opendir(dirname);
+	if(dir == NULL)
+		return;
 	struct dirent* dent;
-	dir = opendir(route);
-	
-	if (dir == 0) return;
-	while (dent = readdir(dir)) {
-		if (dent->d_name[0] != '.') 
+	while((dent = readdir(dir)) != NULL)
+	{
+		if(dent->d_type == DT_DIR)
 		{
-			char buf[90000];
-			strcpy(buf, route);
-			if (dent->d_type == isFolder) 
-				printname(tab, dent->d_name);
-				function(tab + 1, strcat(strcat(buf, dent->d_name), "/"));
+			if(dent->d_name[0] == '.')
+				continue;
+			printname(tabs,dent->d_name);
+			char buf[256];
+			strcpy(buf, dirname);
+			create_tree(strcat(strcat(buf, dent->d_name), "/"),tabs + 1);
 		}
+
 	}
-	closedir(dir);
+	close(dir);
 }
+
+
 
 
 int main (int argc, char *argv[])
 {
 	if (argc == 1)
-		function(0, "/");
+		create_tree("/",0);
 	else
-		function(0, argv[1]);
+		create_tree(argv[1],0);
 	return 0;
 }
